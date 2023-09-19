1b156135ce2cfd9df4ad669994176d663bca8819
Rinat <r.z.zaripov@gmail.com>
2023-09-19 18:22:31 +0300

    firs commit

---
+++ components/header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Header () {
+    return (
+        <header>
+            <Link href='/'>
+                Home
+            </Link>
+            <Link href='/blog'>
+                Blog
+            </Link>
+            <Link href='/about'>
+                About
+            </Link>
+        </header>
+    )
+}
